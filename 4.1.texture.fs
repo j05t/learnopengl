dcd9785f2a071c30460c949702733e474ffdd70b
total-recall <josef.steppan@gmail.com>
2019-08-06 14:18:51 +0200

    multiply fragment color with position

--- 4.1.texture.fs
+++ 4.1.texture.fs
@@ -4,6 +4,8 @@ out vec4 FragColor;
 in vec3 ourColor;
 in vec2 TexCoord;
 
+in vec4 ourPos;
+
 // texture sampler
 uniform sampler2D texture1;
 uniform sampler2D texture2;
@@ -12,6 +14,6 @@ uniform float mixFactor;
 
 void main()
 {
-    FragColor = mix(texture(texture1, TexCoord), texture(texture2, -TexCoord), mixFactor);
+    FragColor = mix(texture(texture1, TexCoord), texture(texture2, -TexCoord), mixFactor) * ourPos;
 }
 
--- 4.1.texture.vs
+++ 4.1.texture.vs
@@ -5,11 +5,13 @@ layout (location = 2) in vec2 aTexCoord;
 
 out vec3 ourColor;
 out vec2 TexCoord;
+out vec4 ourPos;
 
 void main()
 {
 	gl_Position = vec4(aPos, 1.0);
 	ourColor = aColor;
+    ourPos = gl_Position;
 	TexCoord = vec2(aTexCoord.x, aTexCoord.y);
 }
 
